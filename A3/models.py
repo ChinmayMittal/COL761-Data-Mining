b056a21870d21075ebd799a2c4ebffda4ecaef6e
ChinmayMittal <chinmay3102@gmail.com>
2023-11-22 20:50:12 +0530

    Graph Regression  Script

--- A3/models.py
+++ A3/models.py
@@ -92,4 +92,62 @@ class GCNClassifier(torch.nn.Module):
         # print(x)
         # x = x[root_mask, :]
         return x
-    
+
+
+class GCNRegressor(torch.nn.Module):
+    def __init__(self, hidden_channels, out_channels, emb_dim):
+        super().__init__()
+        self.conv1 = GATConv(emb_dim, hidden_channels, heads = 2)
+        self.conv2 = GATConv(2*hidden_channels, hidden_channels, heads = 1)
+        self.edge_linear = torch.nn.Linear(emb_dim, 1)
+
+        self.fc1 = torch.nn.Linear(hidden_channels, 64)
+        self.bn1 = torch.nn.BatchNorm1d(64)
+
+        self.fc2 = torch.nn.Linear(64, 32)
+        self.bn2 = torch.nn.BatchNorm1d(32)
+
+        self.fc3 = torch.nn.Linear(32, 16)
+        self.bn3 = torch.nn.BatchNorm1d(16)
+
+        self.fc4 = torch.nn.Linear(16, out_channels)
+        self.node_encoder = NodeEncoder(emb_dim)
+        self.edge_encoder = EdgeEncoder(emb_dim)
+
+        #Initializations
+
+        init.kaiming_uniform_(self.fc1.weight, mode='fan_in', nonlinearity='relu')
+        init.kaiming_uniform_(self.fc2.weight, mode='fan_in', nonlinearity='relu')
+        init.kaiming_uniform_(self.fc3.weight, mode='fan_in', nonlinearity='relu')
+        init.kaiming_uniform_(self.fc4.weight, mode='fan_in', nonlinearity='relu')
+        torch.nn.init.xavier_uniform_(self.edge_linear.weight)
+
+    def forward(self, x, edge_index, edge_attr, batch, size):
+        x = self.node_encoder(x.to(torch.long))
+        edge_attr = self.edge_encoder(edge_attr.to(torch.long))
+        temp = self.edge_linear(edge_attr)
+        temp = torch.relu(temp)
+        x = self.conv1(x, edge_index, temp).relu()
+        x = self.conv2(x, edge_index, temp).relu()
+        x = global_mean_pool(x, batch)
+        # print(x)
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = torch.relu(x)
+
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = torch.relu(x)
+
+        x = self.fc3(x)
+        x = self.bn3(x)
+        x = torch.relu(x)
+
+        x = self.fc4(x)
+        # x = torch.relu(x)
+        
+        # print(x)
+        # x = F.sigmoid(x)
+        # print(x)
+        # x = x[root_mask, :]
+        return x
---
+++ A3/regression.py
@@ -0,0 +1,92 @@
+import torch
+from evaluate import Evaluator
+from dataset import GraphDataset
+from models import GCNRegressor
+from torch_geometric.loader import DataLoader
+
+
+BATCH_SIZE = 128
+NUM_EPOCHS = 100
+
+X_train = GraphDataset("./dataset/dataset_1/train")
+X_val = GraphDataset("./dataset/dataset_1/valid")
+
+train_loader = DataLoader(X_train, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(X_val, batch_size=len(X_val), shuffle=True)
+
+# if torch.cuda.is_available():
+#     device = torch.device('cuda')
+# elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+#     device = torch.device('mps')
+# else:
+#     device = torch.device('cpu')
+
+device = torch.device('cpu')
+
+model = GCNRegressor(32, 1, 64)
+optimizer = torch.optim.Adam(model.parameters(),lr=0.001)
+
+loss_fun = torch.nn.MSELoss()
+optimizer.zero_grad()
+NUM_EPOCHS = 100
+
+for epoch in range(NUM_EPOCHS):
+    model.train()
+    model.to(device)
+    total_loss = 0.0
+    for i, batch in enumerate(train_loader):
+        # Move batch to device
+        batch.to(device)
+        # Forward pass
+        out = model(batch.x, batch.edge_index.to(torch.long), batch.edge_attr, batch.batch, batch.y.shape[0])
+        # Calculate and print loss
+        # print(batch.y.shape[0])
+        optimizer.zero_grad()
+        loss = loss_fun(out.reshape(1, -1), batch.y.reshape(1, -1))
+        # print(float(loss))
+
+        # Backward pass and optimization
+        loss.mean().backward(retain_graph=True)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+        total_loss+=loss.item()
+    # scheduler.step()
+    average_loss = total_loss / len(train_loader)
+
+     # Validation phase
+    model.eval()
+    with torch.no_grad():
+        total_bce = 0.0
+        
+        for i, batch in enumerate(val_loader):
+             # Move batch to device
+            batch.to(device)
+            # Forward pass
+            out = model(batch.x, batch.edge_index.to(torch.long), batch.edge_attr, batch.batch, batch.y.shape[0])
+            loss = loss_fun(out.reshape(1, -1), batch.y.reshape(1, -1))
+            loss = loss.mean()
+
+            total_bce += loss.item()
+
+        average_bce_val = total_bce / len(val_loader)
+
+    # Print logs
+    print(f"Epoch {epoch+1}/{NUM_EPOCHS}, Train Loss: {average_loss:.4f} , Val MSE: {average_bce_val:.4f}")
+# Make sure to clear the computation graph after the loop
+torch.cuda.empty_cache()
+
+evaluator = Evaluator('dataset-1')
+for i, batch in enumerate(val_loader):
+    model.eval()
+    # Move batch to device
+    batch.to(device)
+    # Forward pass
+    y_pred = model(batch.x, batch.edge_index.to(torch.long), batch.edge_attr, batch.batch, batch.y.shape[0])
+    y_true = batch.y
+    y_true = y_true.unsqueeze(1)
+
+    input_dict = {'y_true': y_true, 'y_pred': y_pred}
+    result = evaluator.eval(input_dict)
+    print(result)
+
+
